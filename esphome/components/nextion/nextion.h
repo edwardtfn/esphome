13256065fc28c2d5bfc99f1c4409a57657d89772
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-11-28 00:00:26 +0100

    Update nextion.h

--- esphome/components/nextion/nextion.h
+++ esphome/components/nextion/nextion.h
@@ -40,7 +40,6 @@ class Nextion : public NextionBase, public display::DisplayBuffer, public Pollin
  public:
   display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
   void draw_pixel_at(int x, int y, Color color) override { this->fill_area(x, y, 1, 1, color); }
-  display::DisplayBuffer::draw_absolute_pixel_internal(int x, int y, Color color) override { this->fill_area(x, y, 1, 1, color); }
 
   /**
    * Set the text of a component to a static string.
@@ -779,6 +778,7 @@ class Nextion : public NextionBase, public display::DisplayBuffer, public Pollin
  protected:
   int get_width_internal() override;
   int get_height_internal() override;
+  display::DisplayBuffer::draw_absolute_pixel_internal(int x, int y, Color color) override { this->fill_area(x, y, 1, 1, color); }
 
   std::deque<NextionQueue *> nextion_queue_;
   std::deque<NextionQueue *> waveform_queue_;
