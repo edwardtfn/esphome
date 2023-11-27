510e0650ad1f21b67f445f315c42e65d8634aeec
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-11-27 21:27:27 +0100

    Override draw_pixel_at

--- esphome/components/nextion/nextion_display.h
+++ esphome/components/nextion/nextion_display.h
@@ -10,6 +10,8 @@ class NextionDisplay;
 class NextionDisplay : public display::DisplayBuffer {
  public:
   display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
+  void draw_pixel_at(int x, int y, Color color) override { this->fill_area(x, y, 1, 1, color); }
+
 
  protected:
   int get_width_internal() override;
