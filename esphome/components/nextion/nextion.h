a3443e9e9733d43ab0a39cbfc9c9a78e3116284b
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-11-27 21:45:51 +0100

    Update nextion.h

--- esphome/components/nextion/nextion.h
+++ esphome/components/nextion/nextion.h
@@ -35,7 +35,7 @@ using nextion_writer_t = std::function<void(Nextion &)>;
 
 static const std::string COMMAND_DELIMITER{static_cast<char>(255), static_cast<char>(255), static_cast<char>(255)};
 
-class Nextion : public NextionBase, public PollingComponent, public uart::UARTDevice, public DisplayBuffer {
+class Nextion : public NextionBase, public PollingComponent, public DisplayBuffer, public uart::UARTDevice {
  public:
   /**
    * Set the text of a component to a static string.
