95116e9f489323cf1410804ace08f349d286bd35
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2023-11-27 23:29:44 +0100

    Update nextion_display.h

--- esphome/components/nextion/nextion_display.h
+++ esphome/components/nextion/nextion_display.h
@@ -1,13 +1,12 @@
 #pragma once
 
-#include "esphome/components/display/display_buffer.h"
 #include "nextion.h"
 
 namespace esphome {
 namespace nextion {
 class NextionDisplay;
 
-class NextionDisplay : public Nextion, public display::DisplayBuffer {
+class NextionDisplay : public Nextion {
  public:
   display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
   void draw_pixel_at(int x, int y, Color color) override { Nextion::fill_area(x, y, 1, 1, color); }
