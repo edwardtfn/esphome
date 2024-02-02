ed5836562daab6a7ed2037ffe6d4939f46bf0625
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-02-02 19:37:55 +0100

    Update nextion_upload.cpp

--- esphome/components/nextion/nextion_upload.cpp
+++ esphome/components/nextion/nextion_upload.cpp
@@ -206,7 +206,7 @@ int Nextion::upload_range(int range_start) {
           int currentChunkSize = std::min(UARTchunkSize, read_len - bytesSent);
           this->write_array(buffer + bytesSent, currentChunkSize);
           bytesSent += currentChunkSize;
-          ESP_LOGV(TAG, "%d of %d bytes sent to Nextion", bytesSent, read_len);
+          ESP_LOGV(TAG, "%d of %d bytes sent to Nextion - %" PRIu32 " available", bytesSent, read_len, this->available());
           // Optional: delay between chunks if required for stability
           //delay(10); // Adjust based on your requirements and testing
       }
